1f095723cad2f9d07c2eacc57828160d1d91d1a7
AadenX2 <144380947+AadenX2@users.noreply.github.com>
2023-09-11 15:40:18 -0500

    New length and changed what print would say. Also a slight change to line 5

--- codegen.py
+++ codegen.py
@@ -2,9 +2,9 @@ import random
 import string
 
 def generate_random_code(length):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     return code
         # Change the 5 in the parenthesis to any higher/lower number if needed
-random_code = generate_random_code(5)
-print ("Your code is:", random_code)
+random_code = generate_random_code(10)
+print ("Code generates is:", random_code)
